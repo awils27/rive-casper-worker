e01bd46159af4b9ada5079ac22f2c1e93826649d
Aiden <awils27@hotmail.com>
2025-10-04 13:31:29 -0600

    removed outdated backend

--- src/handlers/list-templates.mjs
+++
@@ -1,8 +0,0 @@
-import { listPlugins } from "../templates/registry.mjs";
-
-export default async function listTemplates() {
-  const list = listPlugins();
-  return new Response(JSON.stringify(list), {
-    headers: { "content-type": "application/json; charset=utf-8" },
-  });
-}
--- src/lib/utils.js
+++
@@ -1,6 +0,0 @@
-// src/lib/utils.js
-export const htmlHeaders = () => ({ "content-type": "text/html; charset=utf-8" });
-export const downloadHeaders = (name, mime) => ({
-  "content-type": mime,
-  "content-disposition": `attachment; filename="${name}"`
-});
